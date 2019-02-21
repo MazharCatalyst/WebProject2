8e0fc417e86b3a862faef2676d3514bb4cf83d92
Admin <Admin@DESIGNER-PC>
2019-02-21 07:48:33 -0500

    Set label value Hello world

--- WebProject2/Default.aspx.vb
+++ WebProject2/Default.aspx.vb
@@ -2,7 +2,7 @@
     Inherits System.Web.UI.Page
 
     Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
-
+        lblhello.Text = "Hello world"
     End Sub
 
 End Class
